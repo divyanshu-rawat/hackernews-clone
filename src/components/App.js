e8a8967d744dced66411bca4f1b46595a2d7b5d0
Rawat <divyanshu.r46956@gmail.com>
2019-11-10 11:43:45 +0100

    Added howtographQL server 👋

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import '../styles/App.css';
 
+
+/*
+
+apollo-client: Where all the magic happens.
+apollo-cache-inmemory: Our recommended cache.
+apollo-link-http: An Apollo Link for remote data fetching.
+apollo-link-error: An Apollo Link for error handling.
+apollo-link-state: An Apollo Link for local state management.
+graphql-tag: Exports the gql function for your queries & mutations.
+react-apollo contains the bindings to use Apollo Client with React.
+graphql contains Facebook’s reference implementation of GraphQL - Apollo Client uses some of its functionality as well.
+
+*/
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -4,9 +4,32 @@ import './styles/index.css';
 import App from './components/App'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:3000'
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
+
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
+// The first thing you have to do when using Apollo is configure your ApolloClient instance.
+// It needs to know the endpoint of your GraphQL API so it can deal with the network connections.
+// The App is wrapped with the higher-order component ApolloProvider that gets passed the client as a prop.
 serviceWorker.unregister();
